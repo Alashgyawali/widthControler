e60803e928bec9f4d8f40eefec99e61077379955
Alashgyawali <alashgyawali0987@gmail.com>
2021-12-28 19:51:46 +0545

    bnm

---
+++ index.js
@@ -0,0 +1,99 @@
+
+    let a=document.querySelector("#div1");
+    let b=document.querySelector("#div2");
+    let c=document.querySelector("#div3");
+    let d=document.querySelector("#div4");
+    let e=document.querySelector("#div5");
+function first(){
+
+   
+        a.style.width="500px";
+        a.style.height="19vh";
+        b.style.height="auto";
+        c.style.height="auto";
+        d.style.height="auto";
+        e.style.height="auto";
+        // console.log(a);
+        b.style.width="100px";
+        c.style.width="100px";
+        d.style.width="100px";
+        e.style.width="100px";
+
+    
+}
+function second(){
+
+   
+        b.style.width="500px";
+        b.style.height="19vh";
+        // console.log(a);
+        a.style.width="100px";
+        a.style.height="auto";
+        c.style.height="auto";
+        d.style.height="auto";
+        e.style.height="auto";
+        c.style.width="100px";
+        d.style.width="100px";
+        e.style.width="100px";
+
+    
+}
+function third(){
+
+   
+        c.style.width="500px";
+        c.style.height="19vh";
+        a.style.height="auto";
+        b.style.height="auto";
+        d.style.height="auto";
+        e.style.height="auto";
+        // console.log(a);
+        a.style.width="100px";
+        b.style.width="100px";
+        d.style.width="100px";
+        e.style.width="100px";
+
+    
+}
+function forth(){
+
+   
+        d.style.width="500px";
+        d.style.height="19vh";
+        // console.log(a);
+        a.style.height="auto";
+        c.style.height="auto";
+        b.style.height="auto";
+        e.style.height="auto";
+        b.style.width="100px";
+        c.style.width="100px";
+        e.style.width="100px";
+        a.style.width="100px";
+
+    
+}
+function last(){
+
+   
+        e.style.width="500px";
+        e.style.height="19vh";
+        a.style.height="auto";
+        c.style.height="auto";
+        d.style.height="auto";
+        b.style.height="auto";
+        // console.log(a);
+        b.style.width="100px";
+        c.style.width="100px";
+        d.style.width="100px";
+        // e.style.width="100px";
+
+    
+}
+
+// function trying(){
+
+//     let a=document.getElementById("div1").innerText;
+     
+    
+   
+// }
